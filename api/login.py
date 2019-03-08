e7acdc966ac3d4e2aa04dd6ef21805317117b299
Amit Markovich <amitmarkovich92@gmail.com>
2019-03-08 17:53:54 +0200

    Finished login

--- api/login.py
+++ api/login.py
@@ -1,13 +1,22 @@
-from flask import request
+from flask import request, jsonify
 import sqlite3
 
 
 def login():
     username = request.json['username']
     password = request.json['password']
-    conn = sqlite3.connect('db.db')
-    conn.execute(
-        'CREATE TABLE Users (username TEXT PRIMARY KEY, password TEXT, firstName TEXT, lastName TEXT, capacity INTEGER)')
-    print("Table created successfully")
-    conn.close()
-    return 'enter'
+    db = sqlite3.connect('db.db')
+    cur = db.cursor()
+    cur_vars = (username, password)
+    cur.execute('SELECT * FROM Users WHERE username=? AND password=?', cur_vars)
+    user = cur.fetchone()
+    status = 'authorized' if user is not None else 'unauthorized'
+    db.close()
+    return jsonify({"body": request.json, "status": status})
+
+# cur.execute("INSERT INTO Users (username,password,firstName,lastName,capacity) VALUES(?, ?, ?, ?,?)",
+#            ('amitmarko', '12345', 'amit', 'markovich', 100))
+# db.commit()
+# db.execute(
+#  'CREATE TABLE Times (username TEXT, carNumber TEXT, enter TEXT, exit TEXT)')
+# print("Table created successfully")
