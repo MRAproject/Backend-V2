065b7654795cf619dc5d48c005595ac175ad130c
Amit Markovich <amitmarkovich92@gmail.com>
2019-03-16 15:00:58 +0200

    Create add car and get all user cars

--- api/get_all_user_cars.py
+++ api/get_all_user_cars.py
@@ -6,8 +6,8 @@ def get_all_user_cars():
     username = request.args.get('username')
     db = sqlite3.connect('db.db')
     cur = db.cursor()
-    cur.execute('SELECT * FROM Cars WHERE username = ?', (username))
-    cars_list = cur.fetchone()
+    cur.execute('SELECT * FROM Cars WHERE username = ?', (username,))
+    cars_list = cur.fetchall()
     status = 'authorized' if cars_list is not None else 'unauthorized'
     db.close()
     return jsonify({"data": cars_list, "status": status})
